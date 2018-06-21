f23e03772b56a5128bf102295b599fd8cf2ccdf9
SerjNF <foreman@inbox.ru>
2018-06-21 23:39:38 +0700

    Изменения по замечаниям

---
+++ Range/src/ru/inbox/foreman/range/Range.java
@@ -0,0 +1,148 @@
+
+package ru.inbox.foreman.range;
+
+/**
+ * Класс Range. Диапазон определён от меньшего числа к большему
+ *
+ * @author SergeyNF
+ * @since 20.06.2018
+ */
+public class Range {
+
+    private double from;
+    private double to;
+
+
+    /**
+     * Конструктор с параметрами
+     *
+     * @param from начало диапазона
+     * @param to   конец диапазона
+     */
+    public Range(double from, double to) {
+        this.from = Math.min(from, to);
+        Math.min(from, to);
+        this.to = Math.max(from, to);
+    }
+
+    /**
+     * Static method
+     * Объединение диапазонов. rangeOne + rangeTwo
+     *
+     * @param range второй диапазон
+     * @return ArrayList  диапазонов. Результат может состоять из однго или дву диапазонов
+     */
+    public Range[] sumOfRanges( Range range) {
+
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+        Range[] resultRange = new Range[2];
+
+        if ((this.from <= rangeFrom && this.to >= rangeFrom) || (this.from <= rangeTo && this.to >= rangeTo)) {
+            resultRange[0] = new Range(Math.min(this.from, rangeFrom), Math.max(this.to, rangeTo));
+            // нет общего диапазона
+        } else {
+            resultRange[0] = this;
+            resultRange[1] = range;
+        }
+        return resultRange;
+    }
+
+    /**
+     * Static method
+     * Вычитание диапазонов. this(A) - range(B)
+     *
+     *
+     * @param range    второй диапазон
+     * @return Массив диапазонов. Результат может состоять из однго или дву диапазонов. Если вычитаем равные диапазоны или из меньшего больший, возвращает null
+     */
+    public Range[] subtractOfRange(Range range) {
+
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+
+        // Если вычитаем равные диапазоны или из меньшего больший, возвращает ноль
+        if (this.from >= rangeFrom && this.to <= rangeTo) {
+            return null;
+        }
+        Range[] resultRange = new Range[2];
+        // Проверка, что В входит в А
+        if (this.from <= rangeFrom && this.to >= rangeTo) {
+
+            if (this.from != rangeFrom) {
+                resultRange[0] = new Range(this.from, rangeFrom);
+            }
+            if (rangeTo != this.to) {
+                resultRange[1] = new Range(rangeTo, this.to);
+            }
+            // Проверка, что А пересечение В
+        } else if (this.from <= rangeFrom && this.to >= rangeFrom) {
+            resultRange[0] = new Range(this.from, rangeFrom);
+            // Проверка, что В пересечение А
+        } else if (this.to >= rangeTo && this.from <= rangeTo) {
+            resultRange[0] = new Range(rangeTo, this.to);
+            // нет общего диапазона
+        } else {
+            resultRange[0] = this;
+            resultRange[1] = range;
+        }
+        return resultRange;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public String rangeToString() {
+        return "(" + from + ", " + to + ")";
+    }
+
+    /**
+     * Длина диапазона
+     *
+     * @return вещественное число.
+     */
+    public double calcLength() {
+        return to - from;
+    }
+
+    /**
+     * Проверка вхождения числа в диапазон
+     *
+     * @param number проверяемое число
+     * @return boolean  результат проверки. true - входит
+     */
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    /**
+     * Пересечение диапазонов.
+     *
+     * @param range Диапазон, проверяемый на пересечение с данным
+     * @return При отсутствии возвращает null, создавая пустую ссылку. В противном случае, диапазон - ообщий для исходных
+     */
+    public Range intersectRanges(Range range) {
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+
+        if (range.isInside(from) || range.isInside(to)) {
+            return new Range(Math.max(from, rangeFrom), Math.min(to, rangeTo));
+        }
+        return null;
+    }
+
+
+}
---
+++ Range/src/ru/inbox/foreman/test/TestRange.java
@@ -0,0 +1,92 @@
+package ru.inbox.foreman.test;
+
+
+import ru.inbox.foreman.range.Range;
+
+/**
+ * Test Method for class Range
+ * .............\------------------F---------------------\
+ * ____________3.2====А======7_____________12====С=====18.5________________>
+ * .................5------В-------10
+ *
+ * @author SergeyNF
+ * @since 20.06.2018
+ */
+public class TestRange {
+
+    public static void main(String[] arg) {
+
+        Range A = new Range(7, 3.2);
+        Range B = new Range(5, 10);
+        Range C = new Range(12, 18.5);
+        Range F = new Range(3.2, 18.5);
+
+        System.out.printf("Диапазон А (%.2f, %.2f)%n", A.getFrom(), A.getTo());
+        System.out.printf("Диапазон B (%.2f, %.2f)%n", B.getFrom(), B.getTo());
+        System.out.printf("Диапазон C (%.2f, %.2f)%n%n", C.getFrom(), C.getTo());
+
+        // Проверка вхождения числа
+        double number = 4.0;
+
+        System.out.printf("Число %.2f %s в диапазон A длинной %.2f%n", number, (A.isInside(number)) ? "входит" : "не входит", A.calcLength());
+        System.out.printf("Число %.2f %s в диапазон В длинной %.2f%n%n", number, (B.isInside(number)) ? "входит" : "не входит", B.calcLength());
+
+        // Проверка пересечения
+        Range D = A.intersectRanges(B);
+        if (D != null) {
+            System.out.printf("Результат пересечения A и В диапазон D (%.2f, %.2f)%n", D.getFrom(), D.getTo());
+        } else {
+            System.out.println("Результат пересечения, А и В не пересекаются%n");
+        }
+
+        Range E = A.intersectRanges(C);
+        if (E != null) {
+            System.out.printf("Результат пересечения A и C диапазон E (%.2f, %.2f)%n%n", E.getFrom(), E.getTo());
+        } else {
+            System.out.println("Результат пересечения, А и C не пересекаются");
+        }
+
+        // Объединение диапазонов
+        sumRanges(A, B);
+        sumRanges(A, F);
+        sumRanges(C, B);
+        sumRanges(A, C);
+
+        // Разность диапазонов
+        diffRange(A, B);
+        diffRange(F, C);
+        diffRange(C, F);
+    }
+
+    private static void sumRanges(Range one, Range two) {
+
+        System.out.printf("Результат объединения диапазонов %s и %s%n", one.rangeToString(), two.rangeToString());
+        Range[] sun = one.sumOfRanges(two);
+
+        for (int i = 0; i < sun.length; ++i) {
+            Range range = sun[i];
+            if (range != null) {
+                System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
+            }
+        }
+        System.out.println();
+    }
+
+    private static void diffRange(Range one, Range two) {
+        System.out.printf("Результат вычитания из диапазона %s диапазона %s%n", one.rangeToString(), two.rangeToString());
+
+        Range[] subtract = one.subtractOfRange(two);
+
+        if (subtract != null) {
+            for (int i = 0; i < subtract.length; ++i) {
+                Range range = subtract[i];
+                if (range != null) {
+                    System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
+                }
+            }
+        } else {
+            System.out.println("Результат, диапазон нулевой длины либо бесконечность");
+        }
+        System.out.println();
+    }
+}
