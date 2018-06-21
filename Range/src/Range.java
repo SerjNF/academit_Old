7354f05106bf6b447ef565dd663d82a0e534234e
SergeyNF <foreman@inbox.ru>
2018-06-21 14:47:11 +0700

    Задача Range. По первой леекции выполнены все задания

--- Range/src/Range.java
+++ Range/src/Range.java
@@ -21,31 +21,100 @@ public class Range {
 
     /**
      * Конструктор с параметрами
+     *
      * @param from начало диапазона
-     * @param to конец диапазона
+     * @param to   конец диапазона
      */
     Range(double from, double to) {
         this.from = from <= to ? from : to;
         this.to = from <= to ? to : from;
+    }
 
+    /**
+     * Static method
+     * Объединение диапазонов. rangeOne + rangeTwo
+     *
+     * @param rangeOne первый диапазон
+     * @param rangeTwo второй диапазон
+     * @return ArrayList  диапазонов. Результат может состоять из однго или дву диапазонов
+     */
+    static ArrayList<Range> sumOfRanges(Range rangeOne, Range rangeTwo) {
+        double rangeOneFrom = rangeOne.getFrom();
+        double rangeOneTo = rangeOne.getTo();
+        double rangeTwoFrom = rangeTwo.getFrom();
+        double rangeTwoTo = rangeTwo.getTo();
+        ArrayList<Range> resultRange = new ArrayList<>();
+
+        if (rangeOne.isInside(rangeTwoFrom) || rangeOne.isInside(rangeTwoTo) || rangeTwo.isInside(rangeOneFrom) || rangeTwo.isInside(rangeOneTo)) {
+            resultRange.add(new Range(rangeOneFrom <= rangeTwoFrom ? rangeOneFrom : rangeTwoFrom, rangeOneTo >= rangeTwoTo ? rangeOneTo : rangeTwoTo));
+            // нет общего диапазона
+        } else {
+            resultRange.add(rangeOne);
+            resultRange.add(rangeTwo);
+        }
+        return resultRange;
+    }
+
+    /**
+     * Static method
+     * Вычитание диапазонов. rangeOne - rangeTwo
+     *
+     * @param rangeOne первый диапазон (A)
+     * @param rangeTwo второй диапазон (B)
+     * @return ArrayList  диапазонов. Результат может состоять из однго или дву диапазонов. Если вычитаем равные диапазоны или из меньшего больший, возвращает ноль
+     */
+    static ArrayList<Range> subtractOfRange(Range rangeOne, Range rangeTwo) {
+        double rangeOneFrom = rangeOne.getFrom();
+        double rangeOneTo = rangeOne.getTo();
+        double rangeTwoFrom = rangeTwo.getFrom();
+        double rangeTwoTo = rangeTwo.getTo();
+        // Если вычитаем равные диапазоны или из меньшего больший, возвращает ноль
+        if (rangeOneFrom >= rangeTwoFrom && rangeOneTo <= rangeTwoTo) {
+            return null;
+        }
+        ArrayList<Range> resultRange = new ArrayList<>();
+        // Проверка, что В входит в А
+        if (rangeOne.isInside(rangeTwoFrom) && rangeOne.isInside(rangeTwoTo)) {
+
+            if (rangeOneFrom != rangeTwoFrom) {
+                resultRange.add(new Range(rangeOneFrom, rangeTwoFrom));
+            }
+            if (rangeTwoTo != rangeOneTo) {
+                resultRange.add(new Range(rangeTwoTo, rangeOneTo));
+            }
+            // Проверка, что А пересечение В
+        } else if (rangeOneFrom <= rangeTwoFrom && rangeOneTo >= rangeTwoFrom) {
+            resultRange.add(new Range(rangeOneFrom, rangeTwoFrom));
+            // Проверка, что В пересечение А
+        } else if (rangeOneTo >= rangeTwoTo && rangeOneFrom <= rangeTwoTo) {
+            resultRange.add(new Range(rangeTwoTo, rangeOneTo));
+            // нет общего диапазона
+        } else {
+            resultRange.add(rangeOne);
+            resultRange.add(rangeTwo);
+        }
+        return resultRange;
     }
 
     double getFrom() {
         return from;
     }
 
-    double getTo() {
-        return to;
-    }
-
     public void setFrom(double from) {
         this.from = from;
+    }
+
+    double getTo() {
+        return to;
     }
 
     void setTo(double to) {
         this.to = to;
     }
 
+    String rangeToString() {
+        return "(" + from + ", " + to + ")";
+    }
 
     /**
      * Длина диапазона
@@ -80,66 +149,6 @@ public class Range {
             return new Range(from >= rangeFrom ? from : rangeFrom, to <= rangeTo ? to : rangeTo);
         }
         return null;
-    }
-
-    /**
-     * Static method
-     * Объединение диапазонов. rangeOne + rangeTwo
-     *
-     * @param rangeOne первый диапазон
-     * @param rangeTwo второй диапазон
-     * @return ArrayList  диапазонов. Результат может состоять из однго или дву диапазонов
-     */
-    static ArrayList<Range> sumOfRanges(Range rangeOne, Range rangeTwo) {
-        double rangeOneFrom = rangeOne.getFrom();
-        double rangeOneTo = rangeOne.getTo();
-        double rangeTwoFrom = rangeTwo.getFrom();
-        double rangeTwoTo = rangeTwo.getTo();
-        ArrayList<Range> resultRange = new ArrayList<>();
-
-        if (rangeOne.isInside(rangeTwoFrom) || rangeOne.isInside(rangeTwoTo)) {
-            resultRange.add(new Range(rangeOneFrom <= rangeTwoFrom ? rangeOneFrom : rangeTwoFrom, rangeOneTo >= rangeTwoTo ? rangeOneTo : rangeTwoTo));
-        // нет общего диапазона
-        } else {
-            resultRange.add(rangeOne);
-            resultRange.add(rangeTwo);
-        }
-        return resultRange;
-    }
-
-    /**
-     * Static method
-     * Вычитание диапазонов. rangeOne - rangeTwo
-     *
-     * @param rangeOne первый диапазон (A)
-     * @param rangeTwo второй диапазон (B)
-     * @return ArrayList  диапазонов. Результат может состоять из однго или дву диапазонов
-     */
-    static ArrayList<Range> subtractOfRange(Range rangeOne, Range rangeTwo) {
-        double rangeOneFrom = rangeOne.getFrom();
-        double rangeOneTo = rangeOne.getTo();
-        double rangeTwoFrom = rangeTwo.getFrom();
-        double rangeTwoTo = rangeTwo.getTo();
-        if(rangeOneFrom == rangeTwoFrom && rangeOneTo == rangeTwoTo){
-            return null;
-        }
-        ArrayList<Range> resultRange = new ArrayList<>();
-        // Проверка, что В входит в А
-        if (rangeOne.isInside(rangeTwoFrom) && rangeOne.isInside(rangeTwoTo)) {
-            resultRange.add(new Range(rangeOneFrom, rangeTwoFrom));
-            resultRange.add((new Range(rangeTwoTo, rangeOneTo)));
-        // Проверка, что А пересечение В
-        } else if (rangeOneFrom <= rangeTwoFrom && rangeOneTo >= rangeTwoFrom) {
-            resultRange.add(new Range(rangeOneFrom, rangeTwoFrom));
-        // Проверка, что В пересечение А
-        } else if (rangeOneTo >= rangeTwoTo && rangeOneFrom <= rangeTwoTo) {
-            resultRange.add(new Range(rangeTwoTo, rangeOneTo));
-        // нет общего диапазона
-        } else {
-            resultRange.add(rangeOne);
-            resultRange.add(rangeTwo);
-        }
-        return resultRange;
     }
 
 
--- Range/src/TestRange.java
+++ Range/src/TestRange.java
@@ -3,10 +3,10 @@ package ru.inbox.foreman;
 import java.util.ArrayList;
 
 /**
- * Test Method
- * \------------------F---------------------\
- * ____________3.2====А======7_____________12====С=====18.5________________________________>
- * 5------В-------10
+ * Test Method for class Range
+ * .............\------------------F---------------------\
+ * ____________3.2====А======7_____________12====С=====18.5________________>
+ * .................5------В-------10
  *
  * @author SergeyNF
  * @since 20.06.2018
@@ -35,72 +35,58 @@ public class TestRange {
         if (D != null) {
             System.out.printf("Результат пересечения A и В диапазон D (%.2f, %.2f)%n", D.getFrom(), D.getTo());
         } else {
-            System.out.println("А и В не пересекаются%n");
+            System.out.println("Результат пересечения, А и В не пересекаются%n");
         }
 
         Range E = A.interceptRange(C);
         if (E != null) {
-            System.out.printf("Результат пересечения A и C диапазон D (%.2f, %.2f)%n%n", E.getFrom(), E.getTo());
+            System.out.printf("Результат пересечения A и C диапазон E (%.2f, %.2f)%n%n", E.getFrom(), E.getTo());
         } else {
-            System.out.println("А и C не пересекаются");
+            System.out.println("Результат пересечения, А и C не пересекаются");
 
         }
 
         // объединение диапазонов
-        System.out.println();
-        System.out.println("Результат объединения диапазонов А и В");
-        ArrayList<Range> sunAandB = Range.sumOfRanges(A, B);
 
-        for (int i = 0; i < sunAandB.size(); ++i) {
-            Range range = sunAandB.get(i);
-            System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
-        }
-        System.out.println();
+        sumRanges(A, B);
+        sumRanges(B, F);
+        sumRanges(C, B);
+        sumRanges(A, C);
 
-        System.out.println("Результат объединения диапазонов А и С");
-        ArrayList<Range> sunAC = Range.sumOfRanges(A, C);
-
-        for (int i = 0; i < sunAC.size(); ++i) {
-            Range range = sunAC.get(i);
-            System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
-        }
-        System.out.println();
 
         // Разность диапазонов
-
-        System.out.println();
-
-        System.out.println("Результат вычитания из А, В");
-
-        ArrayList<Range> subtractAB = Range.subtractOfRange(A, B);
-
-        if (subtractAB != null) {
-            for (int i = 0; i < subtractAB.size(); ++i) {
-                Range range = subtractAB.get(i);
-                System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
-            }
-        } else {
-            System.out.println("Результат, диапазон нулевой длины");
-        }
-        System.out.println();
-
-
-        System.out.printf("Результат вычитания из F(%.2f, %.2f), B%n", F.getFrom(), F.getTo());
-
-        ArrayList<Range> subtractFB = Range.subtractOfRange(F, B);
-
-        if (subtractFB != null) {
-
-            for (int i = 0; i < subtractFB.size(); ++i) {
-                Range range = subtractFB.get(i);
-                System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
-            }
-        } else {
-            System.out.println("Результат, диапазон нулевой длины");
-
-        }
-
-        System.out.println();
+        diffRange(A, B);
+        diffRange(F, C);
+        diffRange(C, F);
 
     }
+
+    private static void sumRanges(Range one, Range two) {
+
+        System.out.printf("Результат объединения диапазонов %s и %s%n", one.rangeToString(), two.rangeToString());
+        ArrayList<Range> sun = Range.sumOfRanges(one, two);
+
+        for (int i = 0; i < sun.size(); ++i) {
+            Range range = sun.get(i);
+            System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
+        }
+        System.out.println();
+    }
+
+    private static void diffRange(Range one, Range two) {
+        System.out.printf("Результат вычитания из диапазонов %s диапазона %s%n", one.rangeToString(), two.rangeToString());
+
+        ArrayList<Range> subtract = Range.subtractOfRange(one, two);
+
+        if (subtract != null) {
+            for (int i = 0; i < subtract.size(); ++i) {
+                Range range = subtract.get(i);
+                System.out.printf("Диапазон %d (%.2f, %.2f)%n", i + 1, range.getFrom(), range.getTo());
+            }
+        } else {
+            System.out.println("Результат, диапазон нулевой длины либо бесконечность");
+        }
+        System.out.println();
+    }
+
 }
