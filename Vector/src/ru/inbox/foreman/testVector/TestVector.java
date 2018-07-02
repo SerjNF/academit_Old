df8b45e487ab51b8ed9b427e48b4b04211a3127d
SerjNF <foreman@inbox.ru>
2018-07-02 16:33:16 +0700

    Задача на вектор.

--- Vector/src/ru/inbox/foreman/testVector/TestVector.java
+++ Vector/src/ru/inbox/foreman/testVector/TestVector.java
@@ -2,6 +2,9 @@ package ru.inbox.foreman.testVector;
 
 import ru.inbox.foreman.vector.Vector;
 
+/**
+ * Testing class Vector
+ */
 public class TestVector {
 
 
@@ -11,11 +14,27 @@ public class TestVector {
         try {
             v1 = new Vector(3, new double[]{1.0, 5, 4});
             v2 = new Vector(2, new double[]{2, 6});
-            System.out.println(v1.toString());
-            System.out.println(v2.toString());
-            System.out.println(v1.addition(v2).toString());
-            System.out.println(v1.subtraction(v2).toString());
-            System.out.println(v2.getVectorLength());
+
+            System.out.printf("размерность вектора v1 = %d%n", v1.getSize());
+            System.out.printf("размерность вектора v2 = %d%n", v2.getSize());
+
+            System.out.printf("вектор v1 = %s%n", v1.toString());
+            System.out.printf("вектор v2 = %s%n", v2.toString());
+            System.out.printf("прибавление к вектору v1 вектора v2 = %s%n", v1.addition(v2).toString());
+            System.out.printf("вычитание из вектора v1 вектора v2 = %s%n", v1.subtraction(v2).toString());
+            System.out.printf("длина вектра v1 = %s%n", v1.getVectorLength());
+            System.out.printf("длина вектра v2 = %s%n", v2.getVectorLength());
+
+            System.out.println("значение вектора v1 по индексу 1 заменено на -5");
+            v1.setComponentVector(1, -5);
+
+            System.out.printf("значение вектора v1 по индексу 1 = %.1f%n", v1.getComponentVector(1));
+            System.out.printf("развернутый вектор v2 = %s%n", v2.reverse().toString());
+            System.out.printf("сложение векторов v1 и v2 = %s%n", Vector.sumVectors(v1, v2));
+            System.out.printf("разность векторов v1 - v2 = %s%n", Vector.diffVectors(v1, v2));
+            System.out.printf("разность векторов v2 - v1 = %s%n", Vector.diffVectors(v2, v1));
+            System.out.printf("скалярное умножение векторов v1 * v2 = %s%n", Vector.multiplicationVectors(v1, v2));
+
         } catch (IllegalArgumentException e) {
             System.out.println("не допустимая длина массива");
         }
--- Vector/src/ru/inbox/foreman/vector/Vector.java
+++ Vector/src/ru/inbox/foreman/vector/Vector.java
@@ -6,13 +6,18 @@ import java.util.Arrays;
  * Класс вектора размерности n
  *
  * @author SergeyNF
- * @since 28.06.2018
+ * @since 02.07.2018
  */
 public class Vector {
 
     private double[] vector;
 
-
+    /**
+     * Конструктор вектора назмерности n.
+     *
+     * @param n размерность
+     * @throws IllegalArgumentException - исключение при размерности ветора меньше либо равной 0
+     */
     public Vector(int n) throws IllegalArgumentException {
         if (n <= 0) {
             throw new IllegalArgumentException("");
@@ -21,18 +26,35 @@ public class Vector {
         }
     }
 
-    public Vector(Vector vector) throws NullPointerException {
+    /**
+     * Конструктор. копирование вектора
+     *
+     * @param vector вектор
+     */
+    public Vector(Vector vector) {
         if (vector == null) {
-            throw new NullPointerException();
+            this.vector = null;
         } else {
             this.vector = Arrays.copyOf(vector.vector, vector.vector.length);
         }
     }
 
+    /**
+     * Конструктор. Заполнение вектора значениями из массива
+     *
+     * @param vector массив со значениями
+     */
     private Vector(double[] vector) {
         this.vector = Arrays.copyOf(vector, vector.length);
     }
 
+    /**
+     * Конструктор. создание массива размерности n и заполнение вектора значениями из массива
+     *
+     * @param n      размерность
+     * @param vector массив со значениями
+     * @throws IllegalArgumentException исключение при длине массива боьшей размерности
+     */
     public Vector(int n, double[] vector) throws IllegalArgumentException {
         if (n >= vector.length) {
             this.vector = new double[n];
@@ -42,22 +64,13 @@ public class Vector {
         System.arraycopy(vector, 0, this.vector, 0, vector.length);
     }
 
+    /**
+     * Получение размерности массива
+     *
+     * @return int, размерность массива
+     */
     public int getSize() {
         return vector.length;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        for (int i = 0; i < vector.length; ++i) {
-            sb.append(vector[i]);
-            if (i < vector.length - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append("}");
-        return sb.toString();
     }
 
     /**
@@ -68,7 +81,7 @@ public class Vector {
      */
     public Vector addition(Vector vector) {
         if (vector == null) {
-            return null;
+            return new Vector(this.vector);
         }
         Vector tempVector;
         if (this.vector.length < vector.vector.length) {
@@ -83,14 +96,14 @@ public class Vector {
     }
 
     /**
-     * вычитание из вектора другого вуктора
+     * Вычитание из вектора другого вуктора
      *
      * @param vector вычитаемый вектор
      * @return новый объект, результирующий вектор
      */
     public Vector subtraction(Vector vector) {
         if (vector == null) {
-            return null;
+            return new Vector(this.vector);
         }
         Vector tempVector;
         if (this.vector.length < vector.vector.length) {
@@ -117,6 +130,56 @@ public class Vector {
         return new Vector(resVector);
     }
 
+    /**
+     * Умножение вектора на скаляр
+     *
+     * @param scalar double, величина на которую умножается вектор
+     * @return Vector, умноженный на скаляр
+     */
+    public Vector multiplicationOnScalar(double scalar) {
+        double[] resVector = new double[this.vector.length];
+        for (int i = 0; i < this.vector.length; ++i) {
+            resVector[i] = this.vector[i] * scalar;
+        }
+        return new Vector(resVector);
+    }
+
+
+    /**
+     * Возврат значения из массива вектора по индексу
+     *
+     * @param pos индекс в массиве
+     * @return значение компанента по индексу
+     * @throws IllegalArgumentException - исключение при значении индекса выходящего за пределами массива
+     */
+    public double getComponentVector(int pos) throws IllegalArgumentException {
+        if (pos > this.vector.length - 1 || pos < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            return this.vector[pos];
+        }
+    }
+
+    /**
+     * Установка значения в массиве вектора по индексу
+     *
+     * @param pos    индекс в массиве
+     * @param number устанавливаемое значение
+     * @throws IllegalArgumentException - исключение при значении индекса выходящего за пределами массива
+     */
+    public void setComponentVector(int pos, double number) throws IllegalArgumentException {
+        if (pos > this.vector.length - 1 || pos < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.vector[pos] = number;
+        }
+    }
+
+    /**
+     * длина вектора
+     *
+     * @return double, длина вектора
+     */
     public double getVectorLength() {
         double vectorLength = 0;
         for (double v : this.vector) {
@@ -126,19 +189,68 @@ public class Vector {
     }
 
     /**
+     * Статический метод сложения 2х векторов
+     *
+     * @param v1 вектор 1
+     * @param v2 вектор 2
+     * @return резулютирующий вектор, с наибольшей разверностью
+     */
+
+    public static Vector sumVectors(Vector v1, Vector v2) {
+        return v1.addition(v2);
+    }
+
+    /**
+     * Статический метод вычитания из вектора v1 вектора v2
+     *
+     * @param v1 вектор 1 - из которого вычитают
+     * @param v2 вектор 2 - вычитаемый
+     * @return результирующий вектор, с наибольшей разверностью
+     */
+    public static Vector diffVectors(Vector v1, Vector v2) {
+        return v1.subtraction(v2);
+    }
+
+    /**
+     * Статик метод скалярного умножения векторов
+     *
+     * @param v1 вектор 1
+     * @param v2 вектор 2
+     * @return везультат скалярного умножения векторов
+     */
+    public static double multiplicationVectors(Vector v1, Vector v2) {
+        if (v1 == null || v2 == null) {
+            return 0.0;
+        }
+        Vector tempVector;
+        if (v1.vector.length < v2.vector.length) {
+            tempVector = new Vector(v2.vector.length, v1.vector);
+
+            return multiplicationVectors(tempVector.vector, v2.vector);
+        }
+        if (v1.vector.length > v2.vector.length) {
+            tempVector = new Vector(v1.vector.length, v2.vector);
+            return multiplicationVectors(v1.vector, tempVector.vector);
+        }
+        return multiplicationVectors(v1.vector, v2.vector);
+    }
+
+
+    /**
      * Функция сложения 2х массивов почленно
      *
      * @param array1 массив 1
      * @param array2 массив 2
      * @return массив, результат сложения
      */
-    private double[] sumArray(double[] array1, double[] array2) {
+    private static double[] sumArray(double[] array1, double[] array2) {
         double[] resArray = new double[array1.length];
         for (int i = 0; i < array1.length; ++i) {
             resArray[i] = array1[i] + array2[i];
         }
         return resArray;
     }
+
 
     /**
      * функция вычитания из массива 1 массива 2 почленно
@@ -153,6 +265,35 @@ public class Vector {
             resArray[i] = array1[i] - array2[i];
         }
         return resArray;
+    }
+
+    /**
+     * функция вычисления суммы произведения сомпанентов массивов
+     *
+     * @param array1 массив 1
+     * @param array2 массив 2
+     * @return double, результат выполнения
+     */
+    private static double multiplicationVectors(double[] array1, double[] array2) {
+        double result = 0;
+        for (int i = 0; i < array1.length; ++i) {
+            result = result + array1[i] * array2[i];
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 0; i < vector.length; ++i) {
+            sb.append(vector[i]);
+            if (i < vector.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
     }
 
     @Override
@@ -172,9 +313,9 @@ public class Vector {
             return false;
         }
         Vector v = (Vector) o;
-        if (this.vector.length != v.vector.length){
-            return false;
-       }
+//        if (this.vector.length != v.vector.length){
+//            return false;
+//       }
         return Arrays.equals(this.vector, v.vector);
     }
 }
