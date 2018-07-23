fc32c032c4f59f139e9d0182ff5c6e0bbbd53809
SerjNF <foreman@inbox.ru>
2018-07-23 22:04:42 +0700

    Домашние задачи по ArrayList

---
+++ ArrayListHome/src/ru/inbox/foreman/deleteEvenNumbers/DeleteEvenNumbers.java
@@ -0,0 +1,21 @@
+package ru.inbox.foreman.deleteEvenNumbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DeleteEvenNumbers {
+    public static void main(String[] arg) {
+
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 7, 5, 6, 7, 6, 8, 9));
+            numbers.removeIf(x -> x%2 == 0);
+
+//        for (int i = 0; i < numbers.size(); ++i){
+//            if(numbers.get(i)%2 == 0){
+//                numbers.remove(i);
+//                i--;
+//            }
+//        }
+        System.out.println(numbers.toString());
+    }
+}
---
+++ ArrayListHome/src/ru/inbox/foreman/readfile/ReadFile.java
@@ -0,0 +1,22 @@
+package ru.inbox.foreman.readfile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] arg){
+        ArrayList<String> strings = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(new FileInputStream("input.txt"))){
+            while (scanner.hasNext()){
+                strings.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(strings.toString());
+    }
+}
