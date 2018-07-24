09deeb24860d0989f7192ff5e9bc687f990f4a8d
SerjNF <foreman@inbox.ru>
2018-07-24 12:12:19 +0700

    Домашние задачи по ArrayList

--- ArrayListHome/src/ru/inbox/foreman/deleteEvenNumbers/DeleteEvenNumbers.java
+++ ArrayListHome/src/ru/inbox/foreman/deleteEvenNumbers/DeleteEvenNumbers.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 
 public class DeleteEvenNumbers {
     public static void main(String[] arg) {
-
-
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 7, 5, 6, 7, 6, 8, 9));
+
         numbers.removeIf(x -> x % 2 == 0);
 
 //        for (int i = 0; i < numbers.size(); ++i){
