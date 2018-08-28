807342da510413a1ef2dda47ed19967edbf8ece6
SergeyNF <foreman@inbox.ru>
2018-08-28 23:04:11 +0700

    Temperature

--- Temperature/src/ru/inbox/foreman/support/DigitFilter.java
+++ Temperature/src/ru/inbox/foreman/support/DigitFilter.java
@@ -8,13 +8,13 @@ import javax.swing.text.DocumentFilter;
 public class DigitFilter extends DocumentFilter {
     @Override
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-        string = string.replaceAll("[^(\\d+.,\\-)]", "");
+        string = string.replaceAll("[^(\\d.,\\-)]", "");
         super.insertString(fb, offset, string, attr);
     }
 
     @Override
     public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-        text = text.replaceAll("[^(\\d+.,\\-)]", "");
+        text = text.replaceAll("[^(\\d.,\\-)]", "");
         super.replace(fb, offset, length, text, attrs);
     }
     }
--- Temperature/src/ru/inbox/foreman/support/ParserToDouble.java
+++ Temperature/src/ru/inbox/foreman/support/ParserToDouble.java
@@ -2,11 +2,11 @@ package ru.inbox.foreman.support;
 
 public class ParserToDouble {
     public static double parseToDouble(String string) {
-        String[] arrayString = string.split("[\\s,. ]+");
-        double k = arrayString[0].charAt(0) == '-' ? -1.0 : 1.0;
+        double k = string.charAt(0) == '-' ? -1.0 : 1.0;
+        String[] arrayString = string.replaceAll("-","").split("[\\s,.]+");
         double before = Double.valueOf(arrayString[0]);
         double after = arrayString.length > 1 ? Double.valueOf(arrayString[1]) / (Math.pow(10.0, arrayString[1].trim().length())) : 0;
-        return (before + after * k);
+        return (before + after) * k;
     }
 }
 
